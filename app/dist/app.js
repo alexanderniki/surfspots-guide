5682fe7368f7728c2be0fe7d0db597ba16a74fb6
Alexander Niki <aalexanderniki@gmail.com>
2022-08-29 17:02:06 +0300

    added new spot

--- app/dist/app.js
+++ app/dist/app.js
@@ -74,23 +74,6 @@ class Spotlist extends HTMLElement {
     }
 
     render() {
-
-        /*this.innerHTML = `
-        <ul>
-            <li><a href="spot-atlantis.html">Атлантис</a></li>
-            <li><a href="spot-battery.html">Батарейная бухта</a></li>
-            <li><a href="spot-bigsands.html">Большие пески</a></li>
-            <li><a href="spot-osinovets.html">Осиновец</a></li>
-            <li><a href="spot-lighthouse.html">Маяк</a></li>
-            <li><a href="spot-motor.html">Бухта Моторная</a></li>
-            <li><a href="spot-riverking.html">Порог в Кингисеппе</a></li>
-            <!--<li>Мыс Флотский</li>
-            <li>Спот "Парковка"</li>
-            <li>Липово</li>
-            <li><a href="">Бухта Желтая</a></li>-->
-        </ul>
-        `*/
-        
         this.appendChild(this.buildList());
     }
 
