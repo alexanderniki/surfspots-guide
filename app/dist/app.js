f16d1074665a04ff3b46b540d4014c0e2b5097f8
Alexander Niki <aalexanderniki@gmail.com>
2022-08-29 15:41:30 +0300

    updated spotlist component. Generating list from data

--- app/dist/app.js
+++ app/dist/app.js
@@ -52,9 +52,30 @@ class Spotlist extends HTMLElement {
         this.render();
     }
 
+    buildList() {
+        let spots = data.spots;
+        let list = document.createElement("ul");
+
+        for (let i = 0; i < spots.length; i++) {
+            // create list
+            if (spots[i].is_active == true) {
+                let item = document.createElement("li");
+                let link = document.createElement("a");
+                let linkText = document.createTextNode(spots[i].name);
+                link.setAttribute("href", spots[i].page_link);
+
+                link.appendChild(linkText);
+                item.appendChild(link);
+                list.appendChild(item);
+            }
+        }
+        console.log(list);
+        return list;
+    }
+
     render() {
 
-        this.innerHTML = `
+        /*this.innerHTML = `
         <ul>
             <li><a href="spot-atlantis.html">Атлантис</a></li>
             <li><a href="spot-battery.html">Батарейная бухта</a></li>
@@ -68,7 +89,9 @@ class Spotlist extends HTMLElement {
             <li>Липово</li>
             <li><a href="">Бухта Желтая</a></li>-->
         </ul>
-        `
+        `*/
+        
+        this.appendChild(this.buildList());
     }
 
 }
