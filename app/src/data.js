712f0d26b77cffb53f6a082f93aa30d45aed98d4
Alexander Niki <aalexanderniki@gmail.com>
2022-09-08 22:15:07 +0300

    added coordinates of spots to the data

--- app/src/data.js
+++ app/src/data.js
@@ -162,6 +162,18 @@
             name: "Бухта Батарейная",
             code: "ruspbbattery",
             page_link: "spot-battery.html",
+            metadata: 
+                {
+                    location: 
+                    {
+                        coordinates: "59.971340, 29.125711",
+                        lat: 59.971340,
+                        long: 29.125711,
+                        map_code: "",  // google maps link here
+                        city: "Санкт-Петербург",
+                        water: "Финский залив",
+                    }
+            }
         },
         {
             id: 3,
@@ -171,6 +183,15 @@
             page_link: "spot-lighthouse.html",
             metadata: 
                 {
+                    location: 
+                    {
+                        coordinates: "60.118084,31.085118",
+                        lat: 60.118084,
+                        long: 31.085118,
+                        map_code: "",  // google maps link here
+                        city: "Санкт-Петербург",
+                        water: "Ладожское озеро",
+                    },
                     labels: [
                         "парковка",
                         "туалет",
@@ -189,6 +210,15 @@
             page_link: "spot-osinovets.html",
             metadata: 
                 {
+                    location: 
+                    {
+                        coordinates: "60.125960, 31.073964",
+                        lat: 60.125960,
+                        long: 31.073964,
+                        map_code: "",  // google maps link here
+                        city: "Санкт-Петербург",
+                        water: "Ладожское озеро",
+                    },
                     labels: [
                         "парковка",
                         "кафе",
@@ -202,6 +232,18 @@
             name: "Большие пески",
             code: "ruspbbigsands",
             page_link: "spot-bigsands.html",
+            metadata:
+                {
+                    location: 
+                    {
+                        coordinates: "60.301528, 28.849028",
+                        lat: 60.301528,
+                        long: 28.849028,
+                        map_code: "",  // google maps link here
+                        city: "Санкт-Петербург",
+                        water: "Финский залив",
+                    },
+                }
         },
         {
             id: 6,
@@ -209,6 +251,18 @@
             name: "Бухта Моторная",
             code: "ruspbmotor",
             page_link: "spot-motor.html",
+            metadata:
+                {
+                    location: 
+                    {
+                        coordinates: "60.979510, 30.285503",
+                        lat: 60.979510,
+                        long: 30.285503,
+                        map_code: "",  // google maps link here
+                        city: "Санкт-Петербург",
+                        water: "Ладожское озеро",
+                    },
+                }
         },
         {
             id: 7,
@@ -237,6 +291,18 @@
             name: "Сосновый Бор",
             code: "ruspbpinery",
             page_link: "spot-pinery.html",
+            metadata:
+                {
+                    location: 
+                    {
+                        coordinates: "59.869876, 29.065328",
+                        lat: 59.869876,
+                        long: 29.065328,
+                        map_code: "",  // google maps link here
+                        city: "Санкт-Петербург",
+                        water: "Финский залив",
+                    },
+                }
         },
         {
             id: 10,
@@ -244,6 +310,18 @@
             name: "Липово",
             code: "ruspblipovo",
             page_link: "spot-lipovo.html",
+            metadata:
+                {
+                    location: 
+                    {
+                        coordinates: "59.929755, 29.055606",
+                        lat: 59.929755,
+                        long: 29.055606,
+                        map_code: "",  // google maps link here
+                        city: "Санкт-Петербург",
+                        water: "Финский залив",
+                    },
+                }
         },
     ],
 
